97433baec66f998ddfdf8ed89358aae99384bbcb
Ashvini <2662881+cooldude77@users.noreply.github.com>
2025-04-04 11:54:00 +0530

    Updating composer.lock

--- src/SilecustFrameworkBundle.php
+++ src/SilecustFrameworkBundle.php
@@ -2,9 +2,18 @@
 
 namespace Silecust\Framework;
 
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class SilecustFrameworkBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $loader = new YamlFileLoader($builder, new FileLocator(__DIR__ . '/../src/Resources/config'));
+        $loader->load('framework.yaml');
 
+    }
 }
