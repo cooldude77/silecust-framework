d7c79adb62bdffa3ec92b52a21bc79ab9ae2162f
Ashvini <2662881+cooldude77@users.noreply.github.com>
2025-06-09 11:28:29 +0530

    Adding a page title constant. That would be used for displaying page title in themes

--- src/Service/Twig/TwigConstants.php
+++ src/Service/Twig/TwigConstants.php
@@ -5,4 +5,5 @@ namespace Silecust\Framework\Service\Twig;
 class TwigConstants
 {
     const string UI_CONTENT_HEADING = 'ui_content_heading';
+    const string UI_WEB_PAGE_TITLE = 'ui_web_page_title';
 }
